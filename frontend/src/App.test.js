607f04db57e679b605158831c7bd637ff9f07019
Luan Daros <darosluan@gmail.com>
2022-07-25 19:16:14 -0300

    init: created aem maven project

--- frontend/src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from "react-dom";
-import App from "./App";
-
-it("[App] renders without problems", () => {
-  const div = document.createElement("div");
-  render(<App />, div);
-});
--- frontend/src/components/RichText/RichText.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from "react-dom";
-import RichText from "./RichText";
-
-it("[Components - RichText] renders without problems", () => {
-  const div = document.createElement("div");
-  render(<RichText />, div);
-});
