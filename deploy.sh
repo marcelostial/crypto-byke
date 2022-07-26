bf4985332a0a349af4165c5144919b17c3f61c53
Luan Daros <darosluan@gmail.com>
2022-07-25 21:05:28 -0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,15 @@ if [ -z "$(git remote | grep -i adobe)" ]; then
   git remote add adobe $REMOTE
 fi
 
+# if there is no develop branch, create it
+if [ -z "$(git branch | grep -i develop)" ]; then
+  git branch -f develop origin/develop
+fi
+
 # checkout to the develop branch
-git checkout origin/$BRANCH
+git checkout $BRANCH
 git pull
-git push adobe $BRANCH
+git push adobe $BRANCH --force
 
 # go back to the last branch
 git checkout -
