d70084c488280212bf119d503193bc1a8e10d650
Cassiano Simas <cassiano.simas@compasso.com.br>
2022-07-27 15:33:06 -0300

    feat: saving mobile progress

--- ui.frontend/src/layouts/Pedalar/pedalar.js
+++ ui.frontend/src/layouts/Pedalar/pedalar.js
@@ -2,29 +2,53 @@ import styles from "./pedalar.module.scss";
 import PropTypes from "prop-types";
 import Title from "../../components/Title/Title";
 import { CarouselReact } from "../../components";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 // import { Carousel } from "react-responsive-carousel";
 // import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Pedalar = ({ maintitle, cards }) => {
   return (
-    <div className={styles["desktop"]}>
-      <div className={styles["container"]}>
-        <div className={styles["subcontainer"]}>
-          <Title text={maintitle} />
-          <div className={styles["cardscontainer"]}>
-            {cards.map(({ icon, title, description }, index) => (
-              <div className={styles["card"]} key={index}>
-                <img src={icon.src} className={styles["icon"]} alt="svg" />
-                <div className={styles["textcontainer"]}>
-                  <p className={styles["title"]}>{title}</p>
-                  <p className={styles["description"]}>{description}</p>
+    <>
+      {/* Desktop View  */}
+      <div className={styles["desktop"]}>
+        <div className={styles["container"]}>
+          <div className={styles["subcontainer"]}>
+            <Title text={maintitle} />
+            <div className={styles["cardscontainer"]}>
+              {cards.map(({ icon, title, description }, index) => (
+                <div className={styles["card"]} key={index}>
+                  <img src={icon.src} className={styles["icon"]} alt="svg" />
+                  <div className={styles["textcontainer"]}>
+                    <p className={styles["title"]}>{title}</p>
+                    <p className={styles["description"]}>{description}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {/* Movile View */}
+      <div className={styles["mobile"]}>
+        <div className={styles["container"]}>
+          <div className={styles["subcontainer"]}>
+            <Title text={maintitle} />
+            <div className={styles["cardscontainer"]}>
+              {cards.map(({ icon, title, description }, index) => (
+                <div className={styles["card"]} key={index}>
+                  <img src={icon.src} className={styles["icon"]} alt="svg" />
+                  <div className={styles["textcontainer"]}>
+                    <p className={styles["title"]}>{title}</p>
+                    <p className={styles["description"]}>{description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
