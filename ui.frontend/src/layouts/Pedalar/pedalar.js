85d1f92a593639c3757c5393ab47518fd9176043
Cassiano Simas <cassiano.simas@compasso.com.br>
2022-07-27 11:48:02 -0300

    feat: desktop styles completed

--- ui.frontend/src/layouts/Pedalar/pedalar.js
+++ ui.frontend/src/layouts/Pedalar/pedalar.js
@@ -1,24 +1,27 @@
 import styles from "./pedalar.module.scss";
 import PropTypes from "prop-types";
 import Title from "../../components/Title/Title";
+import { CarouselReact } from "../../components";
 // import { Carousel } from "react-responsive-carousel";
 // import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Pedalar = ({ maintitle, cards }) => {
   return (
-    <div className={styles["container"]}>
-      <div className={styles["subcontainer"]}>
-        <Title text={maintitle} />
-        <div className={styles["cardscontainer"]}>
-          {cards.map(({ icon, title, description }, index) => (
-            <div className={styles["card"]}>
-              <img src={icon.src} className={styles["icon"]} alt="svg" />
-              <div className={styles["textcontainer"]}>
-                <p className={styles["title"]}>{title}</p>
-                <p className={styles["description"]}>{description}</p>
+    <div className={styles["desktop"]}>
+      <div className={styles["container"]}>
+        <div className={styles["subcontainer"]}>
+          <Title text={maintitle} />
+          <div className={styles["cardscontainer"]}>
+            {cards.map(({ icon, title, description }, index) => (
+              <div className={styles["card"]} key={index}>
+                <img src={icon.src} className={styles["icon"]} alt="svg" />
+                <div className={styles["textcontainer"]}>
+                  <p className={styles["title"]}>{title}</p>
+                  <p className={styles["description"]}>{description}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
