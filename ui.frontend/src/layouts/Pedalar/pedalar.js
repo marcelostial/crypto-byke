3bb2c401984fa805287ea915ffff71a0f3fc97c5
Cassiano Simas <cassiano.simas@compasso.com.br>
2022-07-26 21:00:49 -0300

    feat: created cards

--- ui.frontend/src/layouts/Pedalar/pedalar.js
+++ ui.frontend/src/layouts/Pedalar/pedalar.js
@@ -1,10 +1,28 @@
 import styles from "./pedalar.module.scss";
 import PropTypes from "prop-types";
+import Title from "../../components/Title/Title";
 // import { Carousel } from "react-responsive-carousel";
 // import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Pedalar = ({ maintitle, cards }) => {
-  return <h1>{maintitle}</h1>;
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["subcontainer"]}>
+        <Title text={maintitle} />
+        <div className={styles["cardscontainer"]}>
+          {cards.map(({ icon, title, description }, index) => (
+            <div className={styles["card"]}>
+              <img src={icon.src} className={styles["icon"]} alt="svg" />
+              <div className={styles["textcontainer"]}>
+                <p className={styles["title"]}>{title}</p>
+                <p className={styles["description"]}>{description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Pedalar.propTypes = {
