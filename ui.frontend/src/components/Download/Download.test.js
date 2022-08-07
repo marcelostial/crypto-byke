768abf5bb05919698aea202ebe6706b9dcaa3e8f
Luan Daros <darosluan@gmail.com>
2022-08-07 16:15:32 -0300

    updated test

--- ui.frontend/src/components/Download/Download.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from "react-dom";
-import Download from "./Download";
-
-it("[Components - Download] renders without problems", () => {
-  const div = document.createElement("div");
-  render(<Download />, div);
-});
--- ui.frontend/src/components/Text/Text.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from "react-dom";
-import Text from "./Text";
-
-it("[Components- Text] renders without problems", () => {
-  const div = document.createElement("div");
-  render(<Text />, div);
-});
