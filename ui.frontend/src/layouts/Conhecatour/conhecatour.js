a22f166e9e700b5abc8725652cec9e04832142c1
Cassiano Simas <cassiano.simas@compasso.com.br>
2022-08-15 19:15:18 -0300

    feature: video customized with button

--- ui.frontend/src/layouts/Conhecatour/conhecatour.js
+++ ui.frontend/src/layouts/Conhecatour/conhecatour.js
@@ -1,36 +1,51 @@
+import { useState, useRef } from "react";
 import PropTypes from "prop-types";
-import "./conhecatour.scss";
 import { Grid } from "../../components/Grid/Grid";
 import Title from "../../components/Title/Title";
+import { useWindowSize } from "../../hooks/useWindowSize";
+import playicon from "../../assets/btn-play.svg";
+import localvideo from "../../assets/TheStolenArtGallery_CompassUOL.mp4.mp4";
+import "./conhecatour.scss";
 
-const conhecatour = ({ titleconhecatour, link }) => {
+const Conhecatour = ({ titleconhecatour, link }) => {
+  const vidRef = useRef();
+  const [paused, setPaused] = useState(true);
+
   return (
     <Grid className="conhecatour--container">
       <Title variant="dark" text={titleconhecatour} />
       <div className="video--container">
-        <iframe
-          width="1250"
-          height="616"
-          src={link}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+        {paused ? (
+          <img
+            src={playicon}
+            alt="play icon"
+            className="icon"
+            onClick={() => {
+              setPaused(!vidRef.current?.paused);
+              vidRef.current.play();
+            }}
+          />
+        ) : (
+          <></>
+        )}
+
+        <video ref={vidRef} className="video" controls>
+          <source src={localvideo} type="video/mp4" />
+        </video>
       </div>
     </Grid>
   );
 };
 
-conhecatour.propTypes = {
+Conhecatour.propTypes = {
   titleconhecatour: PropTypes.string,
   link: PropTypes.string,
 };
 
-conhecatour.defaultProps = {
+Conhecatour.defaultProps = {
   titleconhecatour: "Conheça o Tour de Terre",
 
   link: `https://www.youtube.com/embed/-EFoXEVCuA8`,
 };
 
-export default conhecatour;
+export default Conhecatour;
