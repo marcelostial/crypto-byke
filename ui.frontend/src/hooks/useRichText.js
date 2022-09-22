d52d7c7dcfd14ba0f2b52a3850bb0273b427784a
marcelostial <marceloaface@outlook.com>
2022-09-22 16:18:40 -0300

    init

--- ui.frontend/src/hooks/useRichText.js
+++
@@ -1,8 +0,0 @@
-import sanitizeHtml from "sanitize-html";
-import sanitizeWhiteList from "../utils/sanitize-html.whitelist";
-
-export function useRichText(text) {
-  return {
-    __html: sanitizeHtml(text, sanitizeWhiteList),
-  };
-}
